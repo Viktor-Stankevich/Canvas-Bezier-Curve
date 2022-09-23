4bd956a1797c7a3efe50d4ed14fdd5818952d075
Viktor Stankevich <VStankevich94@gmail.com>
2022-09-23 15:28:07 +0700

    Created a function for create Bezier Curve

--- src/bezierCurve.js
+++ src/bezierCurve.js
@@ -47,13 +47,31 @@ class DrawCurve extends CreateCanvas {
       (options[i].figure == 'Square') ? square(this.ctx, options[i]) : false;
       
       function bezierCurve(ctx, options) {
-        (options.quadraticCurve == true) ? quadraticCurve(ctx, options) : false ;
+        (options.quadraticCurve == true && (options.bezierCurve == false || !options.bezierCurve)) ? quadraticCurve(ctx, options) : 
+        (options.bezierCurve == true && (options.quadraticCurve == false || !options.quadraticCurve)) ? bezierCurve(ctx, options) : false;
         
         function quadraticCurve(ctx, options) {
-          console.log(options.endX)
           ctx.beginPath();
           ctx.moveTo(options.startX, options.startY);
           ctx.quadraticCurveTo(options.controlPoint[0].controlX,options.controlPoint[0].controlY, options.endX, options.endY);
+          (options.strokeColor) ? ctx.strokeStyle = options.strokeColor : false;
+          (options.fillColor) ? ctx.fillStyle = options.fillColor : false;
+          (options.fillColor) ? ctx.fill() : false;
+          ctx.stroke();
+        }
+
+        function bezierCurve(ctx, options) {
+          ctx.beginPath();
+          ctx.moveTo(options.startX, options.startY);
+          const controls = {x : [], y : []};
+          for (let i = 0; i < options.controlPoint.length; i++) {
+            console.log(options.endY)
+            let _controlX = options.controlPoint[i].controlX;
+            let _controlY = options.controlPoint[i].controlY;
+            controls.x.push(_controlX);
+            controls.y.push(_controlY);
+          }
+          ctx.bezierCurveTo(controls.x[0], controls.y[0], controls.x[1], controls.y[1], options.endX, options.endY);
           (options.strokeColor) ? ctx.strokeStyle = options.strokeColor : false;
           (options.fillColor) ? ctx.fillStyle = options.fillColor : false;
           (options.fillColor) ? ctx.fill() : false;
